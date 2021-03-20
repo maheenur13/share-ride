2adacc34f49f5bf042b53b3c820f792a717f7918
maheenur13 <differentmahee13@gmail.com>
2021-03-20 20:54:03 +0600

    adding map

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Destination.css';
 
 import GoogleMap from '../GoogleMap/GoogleMap';
 
 
-const Destination = () => {
+const Destination = (props) => {
+    const [destinationInfo,setDestinationInfo]= useState({
+        isSearchedCliked:false,
 
+    });
+    const handleForm=e=>{
+        if(e.target.name==='from'){
+        const newInfo ={...destinationInfo};
+        newInfo.from=e.target.value;
+        setDestinationInfo(newInfo);
+        }
+        if(e.target.name==='to'){
+        const newInfo ={...destinationInfo};
+        newInfo.to=e.target.value;
+        setDestinationInfo(newInfo);
+        }
+    }
+console.log('valuess',props)
     const handleSubmit=(e) => {
-        console.log('submitted');
+        const newInfo = {...destinationInfo};
+        newInfo.isSearchedCliked=true;
+        setDestinationInfo(newInfo);
         e.preventDefault();
     }
     return (
         <div className="destination-page-design">
             <div className="form-box">
-                <form className="form-style">
+                {!destinationInfo.isSearchedCliked ? <form onSubmit={handleSubmit} className="form-style">
                     <label for="from">Pick From</label>
-                    <input name="from" type="text" defaultValue="Mirpur-11"></input>
-                    <label style={{marginTop:'10px'}} for="to">Pick To</label>
-                    <input name="to" type="text" defaultValue="Dhanmondi"></input>
-                    <input style={{marginTop:'15px',backgroundColor:'#FFA500',border:'none'}} type="submit" value="search" onClick={handleSubmit}></input>
-                </form>
+                    <input onBlur={handleForm} name="from" type="text" placeholder="Mirpur-11" required></input>
+                    <label  style={{marginTop:'10px'}} for="to">Pick To</label>
+                    <input onBlur={handleForm} name="to" type="text" placeholder="Dhanmondi" required></input>
+                    <input style={{marginTop:'15px',backgroundColor:'#FFA500',border:'none'}} type="submit" value="search" ></input>
+                </form>:
+                <div>
+                    <p>{destinationInfo.from}</p>
+                    <p>{destinationInfo.to}</p>
+                </div>
+                }
             </div>
             <div className="map-box">
                 <GoogleMap  ></GoogleMap>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import './Home.css';
 const Home = (props) => {
+    const history=useHistory();
     // console.log(props.data)
-    const { rideName, rideImage } = props.data;
+    const { id,rideName, rideImage } = props.data;
     console.log(rideImage);
+    const handleOnClick=elementId=>{
+        const url=`destination/${rideName}/${elementId}`;
+        history.push(url);
+    }
     return (
         
-            <Link to="/destination" className="ride-boxes">
+            <Link onclick={() =>handleOnClick(id)} to="/destination" className="ride-boxes">
                 <div style={{ width: '100%' }}>
                     <img className="images-style" src={rideImage} alt="" />
                 </div>
